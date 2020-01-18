80354f33344d84ab453f2c1bf13f6aa67266402c
xrdnk <xrdnk1010@gmail.com>
2020-01-18 15:28:10 +0900

    Chapter 3 done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon CallBacks
+    /// <summary>
+    /// プレイヤー退出時
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+    #endregion
+
+    #region Public Methods
+    /// <summary>
+    /// 退出する
+    /// </summary>
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    #endregion
+}
